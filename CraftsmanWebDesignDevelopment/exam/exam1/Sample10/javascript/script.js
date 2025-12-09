eef503935f55c7b3b6d6b6bd8fc86560f9068edd
nmhyrhn <gbs06193@gmail.com>
2025-12-09 11:33:19 +0900

    Add files via upload

---
+++ CraftsmanWebDesignDevelopment/exam/exam1/Sample10/javascript/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+
+  $('.navi>li').mouseover(function(){
+    $(this).find('.submenu').stop().slideDown(500);
+  }).mouseout(function(){
+    $(this).find('.submenu').stop().slideUp(500);
+  })
+
+
+
+
+});
