1a2cf39f0957dc844817592da76a4973c2863b29
nmhyrhn <gbs06193@gmail.com>
2025-12-09 11:51:53 +0900

    Delete CraftsmanWebDesignDevelopment/Practice3 directory

--- CraftsmanWebDesignDevelopment/Practice3/tab2/javascript/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('.cont ul li').click(function () {
-    $(this).parent().accClass("active")
-    .siblings().removeClass('active');
-    return false;
-  });
-});
